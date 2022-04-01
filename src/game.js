4d34b0b4b890e957030fbf6e3c8e1d4a02d2a627
phat_sumo <ethanjpayne1@gmail.com>
2022-04-01 17:49:20 -0600

    update bootstrap, fix flag.js filename, add rock

--- src/game.js
+++ src/game.js
@@ -11,5 +11,10 @@ game.initialize = () => {
   game.bigBlue2 = game.createBigBlue();
   game.bigBlue2.components.position = game.components.Position({x: 200, y: 100});
   game.entities[game.bigBlue2.id] = game.bigBlue2;
+
+  game.rock = game.createRock();
+  game.rock.components.position = game.components.Position({x: 200, y: 200});
+  game.entities[game.rock.id] = game.rock;
+
   requestAnimationFrame(game.loop);
 }
